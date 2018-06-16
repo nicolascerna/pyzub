cbac9195f38280083a079d77d621cb7429ed64f3
Nicolás Cerna <nicolascerna@uc.cl>
2018-06-15 22:29:05 -0400

    Added requeriments to setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=[
+        'Click',
+        'chardet',
+        'tqdm',
+    ]
     entry_points={
         'console_scripts': ['pyzub=pyzub.cli:main', ],
     },
